d62eb356be82f7ae1d38a3602c586293e1ef63c5
yuanyimail1005 <yuanyimail1005@gmail.com>
2025-05-15 07:20:54 -0700

    commit8

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -13,9 +13,6 @@ FINDER_APP_DIR=$(realpath $(dirname $0))
 ARCH=arm64
 CROSS_COMPILE=aarch64-none-linux-gnu-
 
-export PATH=/home/eric/Code/EmbedLinux/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/bin:/home/eric/Code/EmbedLinux/assignment-3-yuanyimail1005:/home/eric/Code/EmbedLinux/assignment-3-yuanyimail1005:/home/eric/Code/EmbedLinux/assignment-3-yuanyimail1005/finder-app:$PATH
-
-
 if [ $# -lt 1 ]
 then
 	echo "Using default directory ${OUTDIR} for output"
